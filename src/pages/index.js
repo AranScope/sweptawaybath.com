f9e78150f435340ce274446aa973e7c0ea8be392
aranscope <aranlong@live.co.uk>
2020-05-30 16:08:01 +0100

    overflow x hidden on mobile nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,6 +24,7 @@ const IndexPage = ({
                 <meta name="description" content={site.siteMetadata.description}/>
             </Helmet>
             <Header
+                className={"overflow-x-hidden"}
                 imageUrl={"https://images.squarespace-cdn.com/content/56ec101db09f95da37e77918/1458317460088-Z27X14MAB5X7CAC0DBOG/SQS_DK_carlos_0226-e.jpg?format=2500w&content-type=image%2Fjpeg"}>
                 <Navigation/>
                 <Quote/>
